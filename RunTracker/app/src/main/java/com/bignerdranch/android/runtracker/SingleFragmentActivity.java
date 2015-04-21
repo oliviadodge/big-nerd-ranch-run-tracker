d33363bcbb133bad702459904e765b8abc7a68d8
Olivia Dodge <oliviadodge@gmail.com>
2015-04-21 12:37:35 -0700

    RunTracker update :wq

---
+++ RunTracker/app/src/main/java/com/bignerdranch/android/runtracker/SingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.runtracker;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+
+
+/**
+ * Created by oliviadodge on 1/26/2015.
+ */
+public abstract class SingleFragmentActivity extends ActionBarActivity {
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId(){
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
+    }
+}
+
